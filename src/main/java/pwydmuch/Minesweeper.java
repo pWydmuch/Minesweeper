94c4dfbce63ff5214577193e42259de711a0fb2f
Patryk Wydmuch <pvydmuch@gmail.com>
2025-05-03 19:56:20 +0200

    Add connection to db

--- src/main/java/pwydmuch/Minesweeper.java
+++ src/main/java/pwydmuch/Minesweeper.java
@@ -7,12 +7,17 @@ import pwydmuch.view.MainView;
 import java.io.*;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
 public class Minesweeper implements Serializable {
 
     @Serial
     private static final long serialVersionUID = 1161474195722226307L;
     private static final Path SAVE_FILE = Path.of("game-state.ser");
+    private static final String DB_URL = "jdbc:sqlite:minesweeper.db";
+
 
     public static void save(MainView sap) {
         Runtime.getRuntime().addShutdownHook(new Thread(() -> {
@@ -24,6 +29,7 @@ public class Minesweeper implements Serializable {
     }
 
     public static void main(String[] args) {
+        connectToDatabase();
         try (var file = new ObjectInputStream(Files.newInputStream(SAVE_FILE))) {
             var gameState = (MainView) file.readObject();
             gameState.getFrame().setVisible(true);
@@ -31,6 +37,15 @@ public class Minesweeper implements Serializable {
             var myButtons = new MyButton[13][13];
             var draw = new Draw(25, myButtons.length, myButtons[0].length);
             new MainView(myButtons, draw).go();
+        }
+    }
+    private static void connectToDatabase() {
+        try (Connection conn = DriverManager.getConnection(DB_URL)) {
+            if (conn != null) {
+                System.out.println("Connected to minesweeper.db SQLite database successfully.");
+            }
+        } catch (SQLException e) {
+            System.out.println("Failed to connect to database: " + e.getMessage());
         }
     }
 }
